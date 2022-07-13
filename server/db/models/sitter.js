76b2d7038f895770ea34bf5a0ea98637f1dea899
copenski <copenski@gmail.com>
2022-07-13 17:35:13 +0800

    DB_main

---
+++ server/db/models/sitter.js
@@ -0,0 +1,67 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Sitter extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      this.belongsTo(User, {
+        foreignKey: 'user_id',
+      });
+    }
+  }
+  Sitter.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cat_flag: {
+      type: DataTypes.BOOLEAN,
+    },
+    dog_flag: {
+      type: DataTypes.BOOLEAN,
+    },
+    experience: {
+      type: DataTypes.INTEGER,
+    },
+    has_pet_flag: {
+      type: DataTypes.BOOLEAN,
+    },
+    has_child: {
+      type: DataTypes.BOOLEAN,
+    },
+    supervision_24: {
+      type: DataTypes.BOOLEAN,
+    },
+    price_per_day: {
+      type: DataTypes.INTEGER,
+    },
+    housing_type: {
+      type: DataTypes.STRING,
+    },
+    walking: {
+      type: DataTypes.BOOLEAN,
+    },
+    staying: {
+      type: DataTypes.BOOLEAN,
+    },
+    price_per_hour: {
+      type: DataTypes.INTEGER,
+    },
+    desc: {
+      type: DataTypes.TEXT,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+    },
+  }, {
+    sequelize,
+    modelName: 'Sitter',
+  });
+  return Sitter;
+};
