2bb9b399dc746d907e9c1b2e935570d2c8c0576d
Kirill <zemlkov07@yandex.ru>
2022-07-18 17:23:36 +0300

    fix bag

--- client/src/pages/SearchResult/SearchResult.jsx
+++ client/src/pages/SearchResult/SearchResult.jsx
@@ -10,6 +10,7 @@ import classes from './SearchResult.module.css'
 import DatePicker from 'react-multi-date-picker';
 import MiniCardSitter from '../MiniCardSitter';
 import { Map, YMaps } from 'react-yandex-maps'
+import { MultiSelect } from 'react-multi-select-component';
 // функция для иконки календаря
 function CustomRangeInput({ openCalendar, value }) {
   let from = value[0] || ""
@@ -81,174 +82,223 @@ export default function SearchResult() {
   };
   // console.log(isLoading);
 
+  //* Для отслкживания изменения множественного выбора возрастов
+  // useEffect(() => {
+  //   setValueInput((prev) => ({ ...prev, Pet_ages: selectedAge.map(el => ({ title: el.value, desc: el.desc })) }))
+  // }, [selectedAge])
+  // //* Для отслкживания изменения множественного выбора размеров
+  // useEffect(() => {
+  //   setValueInput((prev) => ({ ...prev, Pet_sizes: selectedSize.map(el => ({ title: el.value, desc: el.desc })) }))
+  // }, [selectedSize])
+
   useEffect(() => {
     // console.log(valueInput);
     // dispatch(search(valueInput))
     // setValueInput((prev) => ({ ...prev, ...value }));
     if (value.length) {
-      setUsers((prev) => ([...prev, ...value]));
+      setUsers([...value]);
     }
 
-  }, [value, valueInput])
+  }, [value])
 
   return (
-    <>
-      <div className='container mx-auto'>
-        <div className="mt-10 flex justify-center ">
-          <form method="POST" className='w-[70%] mt-10'>
-            <div className="shadow overflow-hidden">
-              <div className='flex justify-center'>
-                <div className='m-3'>
-                  <input className='m-1' type="radio" id="dog" name="type" value="Собака" checked={valueInput.radioValue === "Собака"} onChange={changeRadioHandler} />
-                  <label htmlFor="dog">Собака</label>
-                </div>
-                <div className='m-3'>
-                  <input className='m-1' type="radio" id="cat" name="type" value="Кошка" checked={valueInput.radioValue === "Кошка"} onChange={changeRadioHandler} />
-                  <label htmlFor="cat">Кошка</label>
-                </div>
+    <div className='container mx-auto'>
+      <div className="mt-10 flex justify-center flex-wrap">
+        <form method="POST" className='w-[70%] mt-10'>
+          <div className="shadow overflow-hidden">
+            <div className='flex justify-center'>
+              <div className='m-3'>
+                <input className='m-1' type="radio" id="dog" name="type" value="Собака" checked={valueInput.radioValue === "Собака"} onChange={changeRadioHandler} />
+                <label htmlFor="dog">Собака</label>
               </div>
-
-              <div className='flex items-center justify-evenly'>
-                <div className="flex">
-                  <div className="mb-3 xl:w-96">
-                    <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">Где искать?</label>
-                    <AddressSuggestions defaultQuery={valueInput.textValue} token="7e47857f6ca620ff5df72ae45b911b78fa0f61e4" value={valueInput.textValue} onChange={changeTextHandler} />
-                  </div>
-                </div>
-
-                <div className='flex flex-col justify-center max-w-xl h-5 items-baseline'>
-                  <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 items-baseline">Даты передержки</label>
-                  <DatePicker value={valueInput.dateFrom}
-                    onChange={(date) => setValueInput({
-                      ...valueInput, dateFrom: `${date[0].year}-${date[0].month}-${date[0].day}`,
-                      dateTo: `${date[1]?.year}-${date[1]?.month}-${date[1]?.day}`
-                    })}
-                    multiple={true}
-                    numberOfMonths={2}
-                    minDate={new Date()}
-                    maxDate={new Date().setDate(90)}
-                    render={<CustomRangeInput />}
-                    range />
-                </div>
-
-                <div>
-                  <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 items-baseline">Тип услуги</label>
-                  <select id="countries" value={valueInput.serviceType} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="Передержка">Передержка</option>
-                    <option value="Выгул">Выгул</option>
-                  </select>
-                </div>
-              </div>
-              <div className="px-4 py-5 bg-white sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                  <div className="flex col-span-2">
-                    <div>
-                      <div className="form-check">
-                        <input className={classes.checkInput} checked={!valueInput.hasPetFlag} type="checkbox" value='' onChange={hasPetEtcHandler} id="flexCheckHasPet" name="has_pet_flag" />
-                        <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckHasPet">
-                          Есть собственный питомец
-                        </label>
-                      </div>
-                      <div className="form-check">
-                        <input className={classes.checkInput} checked={!valueInput.hasChild} type="checkbox" value="" onChange={hasPetEtcHandler} id="flexCheckHasChild" name="has_child" />
-                        <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckHasChild">
-                          Есть дети
-                        </label>
-                      </div>
-                      <div className="form-check">
-                        <input className={classes.checkInput} checked={!valueInput.supervision} type="checkbox" value="" onChange={hasPetEtcHandler} id="flexCheck24" name="supervision_24" />
-                        <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheck24">
-                          Постоянный присмотр
-                        </label>
-                      </div>
-
-                      <label className="block text-left col-span-1">
-                        <span className="text-gray-700">Тип жилья</span>
-                        <select className={classes.formControl} value={valueInput.housingType} onChange={hasPetEtcHandler} name="housing_type">
-                          <option value="Квартира">Квартира</option>
-                          <option value="Частный дом">Частный дом</option>
-                        </select>
-                      </label>
-
-                    </div>
-                  </div>
-
-                  <label className="block text-left col-span-2">
-                    <span className="text-gray-700">Размер питомца</span>
-                    <select className={classes.formControl} onChange={hasPetEtcHandler} multiple name="pet_size">
-                      <option>Маленький</option>
-                      <option>Средний</option>
-                      <option>Большой</option>
-                    </select>
-                  </label>
-
-                  <label className="block text-left col-span-2">
-                    <span className="text-gray-700">Возраст питомца</span>
-                    <select className={classes.formControl} onChange={hasPetEtcHandler} multiple name="pet_age">
-                      <option>Щенок</option>
-                      <option>Взрослый</option>
-                      <option>Старый</option>
-                    </select>
-                  </label>
-
-                  <div className="flex col-span-1">
-                    <div className="mb-3">
-                      <label htmlFor="exampleNumber0" className="form-label inline-block mb-2 text-gray-700">Опыт присмотра (лет)</label>
-                      <input
-                        onChange={hasPetEtcHandler}
-                        type="number"
-                        className={classes.formControl}
-                        id="exampleNumber0"
-                        name="experience"
-                      />
-                    </div>
-                  </div>
-
-                  <div className='col-span-2'>
-                    <div className="mb-3">
-                      <label htmlFor="priceDay" className="form-label inline-block mb-2 text-gray-700">Цена за сутки</label>
-                      <input
-                        onChange={hasPetEtcHandler}
-                        type="number"
-                        className={classes.formControl}
-                        id="priceDay"
-                        name="price_per_day"
-                      />
-                    </div>
-                  </div>
-
-                  <div className='col-span-2'>
-                    <div className="mb-3">
-                      <label htmlFor="priceHour" className="form-label inline-block mb-2 text-gray-700">Цена за час</label>
-                      <input
-                        onChange={hasPetEtcHandler}
-                        type="number"
-                        className={classes.formControl}
-                        id="priceHour"
-                        name="price_per_hour"
-                      />
-                    </div>
-                  </div>
-
-                </div>
+              <div className='m-3'>
+                <input className='m-1' type="radio" id="cat" name="type" value="Кошка" checked={valueInput.radioValue === "Кошка"} onChange={changeRadioHandler} />
+                <label htmlFor="cat">Кошка</label>
               </div>
             </div>
-          </form >
+
+            <div className='flex items-center justify-evenly'>
+              <div className="flex">
+                <div className="mb-3 xl:w-96">
+                  <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">Где искать?</label>
+                  <AddressSuggestions defaultQuery={valueInput.textValue} token="7e47857f6ca620ff5df72ae45b911b78fa0f61e4" value={valueInput.textValue} onChange={changeTextHandler} />
+                </div>
+              </div>
+
+              <div className='flex flex-col justify-center max-w-xl h-5 items-baseline'>
+                <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 items-baseline">Даты передержки</label>
+                <DatePicker value={valueInput.dateFrom}
+                  onChange={(date) => setValueInput({
+                    ...valueInput, dateFrom: `${date[0].year}-${date[0].month}-${date[0].day}`,
+                    dateTo: `${date[1]?.year}-${date[1]?.month}-${date[1]?.day}`
+                  })}
+                  multiple={true}
+                  numberOfMonths={2}
+                  minDate={new Date()}
+                  maxDate={new Date().setDate(90)}
+                  render={<CustomRangeInput />}
+                  range />
+              </div>
+
+              <div>
+                <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 items-baseline">Тип услуги</label>
+                <select id="countries" value={valueInput.serviceType} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="Передержка">Передержка</option>
+                  <option value="Выгул">Выгул</option>
+                </select>
+              </div>
+            </div>
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="flex col-span-2">
+                  <div>
+                    <div className="form-check">
+                      <input className={classes.checkInput} checked={!valueInput.hasPetFlag} type="checkbox" value='' onChange={hasPetEtcHandler} id="flexCheckHasPet" name="has_pet_flag" />
+                      <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckHasPet">
+                        Есть собственный питомец
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input className={classes.checkInput} checked={!valueInput.hasChild} type="checkbox" value="" onChange={hasPetEtcHandler} id="flexCheckHasChild" name="has_child" />
+                      <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckHasChild">
+                        Есть дети
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input className={classes.checkInput} checked={!valueInput.supervision} type="checkbox" value="" onChange={hasPetEtcHandler} id="flexCheck24" name="supervision_24" />
+                      <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheck24">
+                        Постоянный присмотр
+                      </label>
+                    </div>
+
+                    <label className="block text-left col-span-1">
+                      <span className="text-gray-700">Тип жилья</span>
+                      <select className={classes.formControl} value={valueInput.housingType} onChange={hasPetEtcHandler} name="housing_type">
+                        <option value="Квартира">Квартира</option>
+                        <option value="Частный дом">Частный дом</option>
+                      </select>
+                    </label>
+
+                  </div>
+                </div>
+
+                <label className="block text-left col-span-2">
+                  <span className="text-gray-700">Размер питомца</span>
+                  <select className={classes.formControl} onChange={hasPetEtcHandler} multiple name="pet_size">
+                    <option>Маленький</option>
+                    <option>Средний</option>
+                    <option>Большой</option>
+                  </select>
+                </label>
+
+                <label className="block text-left col-span-2">
+                  <span className="text-gray-700">Возраст питомца</span>
+                  <select className={classes.formControl} onChange={hasPetEtcHandler} multiple name="pet_age">
+                    <option>Щенок</option>
+                    <option>Взрослый</option>
+                    <option>Старый</option>
+                  </select>
+                </label>
+
+                {/* <div className="text-left col-span-2">
+                    <label className="form-label inline-block mb-2 text-gray-700">Возраст питомца</label>
+                    <MultiSelect
+                      options={[{ label: 'Щенок (до 1 года)', value: 'Щенок', desc: '(до 1 года)' },
+                      { label: 'Взрослый (от 1 года до 7 лет)', value: 'Взрослый', desc: '(от 1 года до 7 лет)' },
+                      { label: 'Старый (от 7 лет)', value: 'Старый', desc: '(от 7 лет)' },
+                      ]}
+                      value={selectedAge}
+                      onChange={setSelectedAge}
+                      disableSearch={true}
+                      overrideStrings={{
+                        "allItemsAreSelected": "Выбраны все размеры",
+                        "selectAll": "Выбрать все",
+                        "selectAllFiltered": "Выбрать все",
+                        "selectSomeItems": "Выберите возраст",
+                      }}
+                    />
+                  </div>
+                  <div className="text-left col-span-2">
+                    <label className="form-label inline-block mb-2 text-gray-700">Размер питомца</label>
+                    <MultiSelect
+                      options={[{ label: 'Маленький (от 1 до 10 кг)', value: 'Маленький', desc: '(от 1 до 10 кг)' },
+                      { label: 'Средний (от 10 до 30 кг)', value: 'Средний', desc: '(от 10 до 30 кг)' },
+                      { label: 'Большой (от 30 кг)', value: 'Большой', desc: '(от 30 кг)' },
+                      ]}
+                      value={selectedSize}
+                      onChange={setSelectedSize}
+                      disableSearch={true}
+                      overrideStrings={{
+                        "allItemsAreSelected": "Выбраны все размеры",
+                        "selectAll": "Выбрать все",
+                        "selectAllFiltered": "Выбрать все",
+                        "selectSomeItems": "Выберите размеры",
+                      }}
+                    />
+                  </div> */}
+
+                <div className="flex col-span-1">
+                  <div className="mb-3">
+                    <label htmlFor="exampleNumber0" className="form-label inline-block mb-2 text-gray-700">Опыт присмотра (лет)</label>
+                    <input
+                      onChange={hasPetEtcHandler}
+                      type="number"
+                      className={classes.formControl}
+                      id="exampleNumber0"
+                      name="experience"
+                    />
+                  </div>
+                </div>
+
+                <div className='col-span-2'>
+                  <div className="mb-3">
+                    <label htmlFor="priceDay" className="form-label inline-block mb-2 text-gray-700">Цена за сутки</label>
+                    <input
+                      onChange={hasPetEtcHandler}
+                      type="number"
+                      className={classes.formControl}
+                      id="priceDay"
+                      name="price_per_day"
+                    />
+                  </div>
+                </div>
+
+                <div className='col-span-2'>
+                  <div className="mb-3">
+                    <label htmlFor="priceHour" className="form-label inline-block mb-2 text-gray-700">Цена за час</label>
+                    <input
+                      onChange={hasPetEtcHandler}
+                      type="number"
+                      className={classes.formControl}
+                      id="priceHour"
+                      name="price_per_hour"
+                    />
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </form >
+        <div className="grid grid-cols-2 gap-2">
           {isLoading ? (
-            <div className="flex items-center justify-center space-x-2">
+            <div className="flex items-center justify-center space-x-2 col-span-1">
               <div className="spinner-border animate-spin inline-block w-12 h-12 border-4 rounded-full" role="status">
                 <span className="visually-hidden">Loading...</span>
               </div>
             </div>
           ) : //MiniCardSitter()
-            users.map((el) => MiniCardSitter(el)) // вот тут ломается. чтобы работало оставаясь на странице поставь ? после users и сохрани
+            <div className='flex col-span-1 flex-wrap'>
+              {users.map((el) => MiniCardSitter(el))}
+            </div>
+            // вот тут ломается. чтобы работало оставаясь на странице поставь ? после users и сохрани
           }
+          <YMaps className="col-span-2">
+            <Map defaultState={maState}></Map>
+          </YMaps>
         </div >
-        <YMaps>
-          <Map defaultState={maState}></Map>
-        </YMaps>
-      </div>
 
-    </>
+      </div>
+    </div>
   )
 }
