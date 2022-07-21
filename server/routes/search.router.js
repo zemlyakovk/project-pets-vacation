1d38671c869ac2806ba27f289284ee5685a52249
copenski <copenski@gmail.com>
2022-07-21 10:05:02 +0800

    rebasing

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Op } = require("sequelize");
 const { Sitter, User, Sitter_date, Address, sequelize, Pet_age, Pet_size } = require('../db/models');
-const { customeWhere } = require('../controller/controller')
+const { customeWhere, formatDate } = require('../controller/controller')
 
 router.get('/', async (req, res) => {
   const sitters = [];
@@ -9,7 +9,6 @@ router.get('/', async (req, res) => {
   const {
     radioValue, dateFrom, dateTo, serviceType, latitude, longitude, distance, sitter_pet_ages, sitter_pet_sizes
   } = req.query;
-  console.log(sitter_pet_ages, sitter_pet_sizes);
 
   const arrAge = sitter_pet_ages?.length ? sitter_pet_ages : ['Щенок', 'Взрослый', 'Старый'];
   const arrSize = sitter_pet_sizes?.length ? sitter_pet_sizes : ['Маленький', 'Большой', 'Средний'];
@@ -20,22 +19,21 @@ router.get('/', async (req, res) => {
   if (dateTo) {
     try {
       // функция, считающая, сколько дней между dateFrom до dateTo
+      const fDateFrom = formatDate(dateFrom);
+      const fDateTo = formatDate(dateTo);
       const getNumberOfDays = function (start, end) {
-        const date1 = new Date(start);
-        const date2 = new Date(end);
-
         // One day in milliseconds
         const oneDay = 1000 * 60 * 60 * 24;
 
         // Calculating the time difference between two dates
-        const diffInTime = date2.getTime() - date1.getTime();
+        const diffInTime = end.getTime() - start.getTime();
 
         // Calculating the no. of days between two dates
         const diffInDays = Math.round(diffInTime / oneDay);
 
         return diffInDays + 1;
       }
-      const numberOfDays = getNumberOfDays(dateFrom, dateTo);
+      const numberOfDays = getNumberOfDays(fDateFrom, fDateTo);
 
       const filterWhere = customeWhere(req.query)
 
@@ -64,7 +62,7 @@ router.get('/', async (req, res) => {
           },
           {
             model: Sitter_date,
-            where: { aval_date: { [Op.between]: [new Date(dateFrom), new Date(dateTo)] } }
+            where: { aval_date: { [Op.between]: [fDateFrom, fDateTo] } }
           },
           {
             model: User,
@@ -88,28 +86,6 @@ router.get('/', async (req, res) => {
       })
 
       const searchResult = sitters.filter((sitter) => sitter.Sitter_dates.length === numberOfDays)
-      // const sitterFilterDate = await Sitter_date.findAll({ where: { aval_date: { [Op.between]: [new Date(dateFrom), new Date(dateTo)] } } });
-      // // console.log('нашли по датам ===>>>', sitterFilterDate);
-      // // формируем массив с массивами. Вложенные массивы сгруппированны по sitter_id
-      // const map = sitterFilterDate.reduce((r, i) => {
-      //   r[i.sitter_id] = r[i.sitter_id] || [];
-      //   r[i.sitter_id].push(i);
-      //   return r;
-      // }, {});
-
-      // const arr1 = [];
-
-      // for (const key in map) {
-      //   arr1.push(map[key]);
-      // }
-
-      // const resultArrSitterID = []; // итоговый массив с айди ситтеров, которые могут сидеть в это время
-
-      // for (let i = 0; i < arr1.length; i++) {
-      //   if (arr1[i].length === numberOfDays) {
-      //     resultArrSitterID.push(arr1[i][0].sitter_id)
-      //   }
-      // }
       res.json(searchResult)
     } catch (err) {
       console.log(err);
