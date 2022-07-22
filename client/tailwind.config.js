e5a3d9f9902c72ffce9d9a25ead29a28d28b58b6
Nick <teolianwork@gmail.com>
2022-07-22 16:23:35 +0300

    font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'amatic': 'Amatic SC',
+        'Oranienbaum': 'Oranienbaum'
+      }
+    },
   },
   plugins: [require("tw-elements/dist/plugin")],
 };
